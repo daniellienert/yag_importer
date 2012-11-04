24b6ef2136432654bb8a2a68e0766e6456ae9730
Daniel Lienert <daniel@lienert.cc>
2012-11-04 18:23:07 +0100

    CHG: Gallery importer

--- Classes/Domain/Importer/Gallery2.php
+++ Classes/Domain/Importer/Gallery2.php
@@ -1,17 +1,19 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: daniel
- * Date: 30.10.12
- * Time: 11:21
- * To change this template use File | Settings | File Templates.
- */
+
+
 class Tx_YagImporter_Domain_Importer_Gallery2 {
+
+	/**
+	 * @var Tx_Extbase_Object_ObjectManager
+	 */
+	protected $objectManager;
+
 
 	/**
 	 * @var Tx_PtExtlist_Domain_DataBackend_DataSource_MySqlDataSource
 	 */
 	protected $dataSource;
+
 
 	/**
 	 * @var array
@@ -31,24 +33,102 @@ class Tx_YagImporter_Domain_Importer_Gallery2 {
 	protected $importConfiguration;
 
 
+	/**
+	 * @var Tx_Yag_Domain_Repository_GalleryRepository
+	 */
+	protected $galleryRepository;
+
+
+	/**
+	 * @var Tx_Yag_Domain_Repository_AlbumRepository
+	 */
+	protected $albumRepository;
+
+
+	/**
+	 * @param Tx_Yag_Domain_Repository_GalleryRepository $galleryRepository
+	 */
+	public function injectGalleryRepository(Tx_Yag_Domain_Repository_GalleryRepository $galleryRepository) {
+		$this->galleryRepository = $galleryRepository;
+	}
+
+
+	/**
+	 * @param Tx_Yag_Domain_Repository_AlbumRepository $albumRepository
+	 */
+	public function injectAlbumRepository(Tx_Yag_Domain_Repository_AlbumRepository $albumRepository) {
+		$this->albumRepository = $albumRepository;
+	}
+
+
+	/**
+	 * @param Tx_Extbase_Object_ObjectManager $objectManager
+	 */
+	public function injectObjectManager(Tx_Extbase_Object_ObjectManager $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+
+	public function init() {
+		$pidDetector = $this->objectManager->get('Tx_Yag_Utility_PidDetector'); /** @var $pidDetector Tx_Yag_Utility_PidDetector */
+		$pidDetector->setPids(array($this->importConfiguration['targetPid']));
+	}
+
 
 	public function import() {
+		$this->buildStructure($this->importConfiguration['importRootUid']);
+		$this->importAlbum($this->plainStructure[440]);
+	}
+
+
+	protected function importAlbum($g2Entity) {
+		$gallery = $this->selectOrCreateGallery($g2Entity);
+
+		if($g2Entity['yagAlbumName']) {
+			$album = $this->addAlbum($g2Entity);
+			$album->setGallery($gallery);
+			$gallery->addAlbum($album);
+		}
 
 	}
 
 
-	protected function addGalleryIfNotExists($g2Entity) {
+	/**
+	 * @param $g2Entity
+	 * @return object|Tx_Yag_Domain_Model_Gallery
+	 */
+	protected function selectOrCreateGallery($g2Entity) {
+		$gallery = $this->galleryRepository->findOneByName($g2Entity['yagGalleryName']);
 
+		if(!$gallery instanceof Tx_Yag_Domain_Model_Gallery) {
+			$gallery = $this->objectManager->get('Tx_Yag_Domain_Model_Gallery'); /** @var Tx_Yag_Domain_Model_Gallery $gallery */
+			$gallery->setName($g2Entity['yagGalleryName']);
+			$gallery->setDescription($g2Entity['yagGalleryDescription']);
+			$gallery->setDate(new DateTime('@'.$g2Entity['yagGalleryDate']));
+			$gallery->setPid($this->importConfiguration['targetPid']);
+		}
+
+		return $gallery;
 	}
 
 
-	protected function addAlbum($g2Entity) {
+	/**
+	 * @param array $g2Entity
+	 * @return object|Tx_Yag_Domain_Model_Album
+	 */
+	protected function addAlbum(array $g2Entity) {
+		$album = $this->objectManager->get('Tx_Yag_Domain_Model_Album'); /** @var Tx_Yag_Domain_Model_Album $album */
+		$album->setDate(new DateTime('@'.$g2Entity['item']['g_originationTimestamp']));
+		$album->setName($g2Entity['yagAlbumName']);
+		$album->setDescription($g2Entity['item']['g_description']);
+		$album->setPid($this->importConfiguration['targetPid']);
 
+		return $album;
 	}
 
 
 	protected function importImages() {
-		
+
 	}
 
 
@@ -65,7 +145,7 @@ class Tx_YagImporter_Domain_Importer_Gallery2 {
 	 * @param int $rootUid
 	 */
 	public function buildStructure($rootUid = 0) {
-		if(count $this->plainStructure == 0) {
+		if(count($this->plainStructure) == 0) {
 			$structure = array();
 			$this->buildStructureRecursive($structure, $rootUid);
 
@@ -104,20 +184,32 @@ class Tx_YagImporter_Domain_Importer_Gallery2 {
 			switch ($level) {
 				case 0:
 					$title = $item['g_title'];
+
 					$yagGalleryName = $item['g_title'];
+					$yagGalleryDate = $item['g_originationTimestamp'];
+					$yagGalleryDescription = $item['g_description'];
+
 					$path =  $item['g_pathComponent'];
 				break;
 
 				case 1:
 					$title = $item['g_title'];
+
 					$yagGalleryName = $this->plainStructure[$parentId]['yagGalleryName'];
+					$yagGalleryDate = $this->plainStructure[$parentId]['yagGalleryDate'];
+					$yagGalleryDescription = $this->plainStructure[$parentId]['yagGalleryDescription'];
+
 					$yagAlbumName = $title;
 					$path =  $this->plainStructure[$parentId]['path'] . '/' . $item['g_pathComponent'];
 				break;
 
 				default:
 					$title = $parentTitle . ' / ' . $item['g_title'];
+
 					$yagGalleryName = $this->plainStructure[$parentId]['yagGalleryName'];
+					$yagGalleryDate = $this->plainStructure[$parentId]['yagGalleryDate'];
+					$yagGalleryDescription = $this->plainStructure[$parentId]['yagGalleryDescription'];
+
 					$yagAlbumName = $title;
 					$path =  $this->plainStructure[$parentId]['path'] . '/' . $item['g_pathComponent'];
 			}
@@ -127,6 +219,8 @@ class Tx_YagImporter_Domain_Importer_Gallery2 {
 				'title' => $title,
 				'item' => $item,
 				'yagGalleryName' => $yagGalleryName,
+				'yagGalleryDescription' => $yagGalleryDescription,
+				'yagGalleryDate' => $yagGalleryDate,
 				'yagAlbumName' => $yagAlbumName,
 				'path' => $path,
 			);
@@ -145,6 +239,7 @@ class Tx_YagImporter_Domain_Importer_Gallery2 {
 	protected function selectChildItems($parentId, $isAlbum = TRUE) {
 		$statement = "
 			SELECT entity.g_id, g_title, g_description, IF(album.g_id, 1,0) as is_album, g_pathComponent,
+			g_originationTimestamp,
 
 			(SELECT COUNT(*)
 			FROM `g2_Item` int_item
